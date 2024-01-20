9e2524a376f3319498e0872c01a9e9da2e557970
Matheus Borri <matheus.escobar90@gmail.com>
2024-01-20 20:12:19 -0300

    style(navigation): adiciona Navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
     fontFamily: {
       body: ["Quicksand"],
     },
+    colors: {
+      primary: "#FF6F61",
+      black: "#000000",
+      white: "#ffffff",
+      gray: "#E8E8E8",
+    },
   },
   plugins: [],
 };
